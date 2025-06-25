490c370bfde2070270b97de2030d3442ba3bcabb
yeni0727 <fullsun9600@naver.com>
2025-06-25 18:15:30 +0900

    up

--- src/components/WeatherMain.jsx
+++ src/components/WeatherMain.jsx
@@ -77,7 +77,7 @@ const WeatherMain = () => {
                      </div>
                   ))
             ) : (
-               <div>로딩중..</div>
+               <div>예보없음</div>
             )}
          </div>
       </div>
